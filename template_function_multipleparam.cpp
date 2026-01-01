b0527ce6c8153c3a43a8427232e82723e80c32d9
bocharerahul17 <102786618+bocharerahul17@users.noreply.github.com>
2026-01-01 15:31:59 +0530

    Add files via upload

---
+++ template_function_multipleparam.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+//template <typename T1, typename T2>      // multiple parameters
+
+// void foo(T1 input1, T2 input2) {
+//     cout<<typeid(input1).name()<<endl;
+//     cout<<typeid(input2).name()<<endl;
+    
+// }
+
+
+template <typename T1, size_t N>         // non object type param Ex. Arrays
+
+void foo(T1 input1) {
+    for(size_t i =0; i<N; i++){
+        cout<<typeid(input1).name()<<endl;
+    }
+    
+    
+}
+
+int main()
+{
+    foo<int, 5>(5);
+
+    return 0;
+}
